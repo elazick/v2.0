632b46aa697d08877690bfe2b8299043e84c9e66
elazick <112683136+elazick@users.noreply.github.com>
2023-01-02 12:31:52 +0200

    Update class.h

--- class.h
+++ class.h
@@ -1,6 +1,5 @@
 #ifndef CLASS_H
 #define CLASS_H
-#endif
 #include <iostream>
 #include <cstdio>
 #include <string>
@@ -24,69 +23,67 @@
 
 class zmogus {
 protected:
-	std::string vardas;
-	std::string pavarde;
+	std::string vardas_;
+	std::string pavarde_;
 
 public:
-	zmogus() {}; //def konstruktorius
+	zmogus(std::string vardas, std::string pavarde) {
+		vardas_ = vardas; pavarde_ = pavarde;
+	};  //konstruktorius
+
+	zmogus() {
+		vardas_ = ""; pavarde_ = "";
+	}
+
+	virtual void Vardas(std::string vardas)  = 0;
+	virtual void Pavarde(std::string pavarde)  = 0;
 
 	//getteriai
-	inline std::string getVardas() const { return vardas; }
-	inline std::string getPavarde() const { return pavarde; }
+	inline std::string getVardas() const { return vardas_; }
+	inline std::string getPavarde() const { return pavarde_; }
 
-	//setteriai
-	inline void setVardas(std::string _vardas) { vardas = _vardas; }
-	inline void setPavarde(std::string _pavarde) { pavarde = _pavarde; }
-
-	~zmogus() {}; //destruktorius
 };
 
 class studentas : public zmogus {
-	std::vector<int> pazymiai;
-	int paz_sk;
-	int sum;
-	double egzas;
-	double galutinis;
+private:
+	std::vector<int> pazymiai_;
+	int paz_sk_;
+	int sum_;
+	double egzas_;
+	double galutinis_;
 
 public:
-	studentas() : galutinis(0) { } //default konstruktorius
-	studentas(std::string vardas, std::string pavarde);
+	studentas() : zmogus() { 
+		pazymiai_ = { 0 }, egzas_ = 0, galutinis_ = 0, sum_ = 0, paz_sk_ = 0;
+	} //konstruktorius
+	studentas(std::string vardas, std::string pavarde, std::vector<int> pazymiai, int egzas);
 
 	//getteriai
-	inline std::vector<int> getPazymiai() const { return pazymiai; }
-	inline int getEgzas() const { return egzas; }
-	inline int getSum() const { return sum; }
-	inline double getRezultatas() const { return galutinis; }
+	inline std::vector<int> getPazymiai() { return pazymiai_; }
+	inline int getEgzas() { return egzas_; }
+	inline int getPaz() { return paz_sk_; }
+	inline int getSum() { return sum_; }
+	inline double getRezultatas() { return galutinis_; }
+	const std::string getVardas() const { return vardas_; }
+	const std::string getPavarde() const { return pavarde_; }
+	const std::vector<int> getPazymiai() const { return pazymiai_; }
+	const int getEgzas() const { return egzas_; }
+	const int getPaz() const { return paz_sk_; }
+	const int getStum() const { return sum_; }
+	const double getRezultatas() const { return galutinis_; }
+
+
 
 	//setteriai
-	inline void setPazymiai(std::vector<int>pazymiai) { studentas::pazymiai = pazymiai; }
-	inline void setSum(int sum) { studentas::sum = sum; }
-	inline void setEgzas(int egzas) { studentas::egzas = egzas; }
-	inline void setRezultatas(double galutinis) { studentas::galutinis = galutinis; }
+	void Vardas(std::string vardas) { vardas_ = vardas; }
+	void Pavarde(std::string pavarde) { pavarde_ = pavarde; }
 
-	//copy constructor
-	studentas(const studentas& other) :
-		pazymiai(other.pazymiai),
-		egzas(other.egzas),
-		paz_sk(other.paz_sk),
-		sum(other.sum),
-		galutinis(other.galutinis) {
-			
-			vardas = other.getVardas();
-			pavarde = other.getPavarde();
-	};
+	inline void Pazymiai(std::vector<int>pazymiai) { pazymiai_ = pazymiai; }
+	inline void Sum(int sum) { sum_ = sum; }
+	inline void Egzas(int egzas) { egzas_ = egzas; }
+	inline void Rezultatas(double galutinis) { galutinis_ = galutinis; }
 
-	//copy assignment operator
-	studentas& operator = (const studentas& other) {
-		vardas = other.vardas;
-		pavarde = other.pavarde;
-		pazymiai = other.pazymiai;
-		paz_sk = other.paz_sk;
-		sum = other.sum;
-		galutinis = other.galutinis;
-		return *this;
-	}
-
+	
 	~studentas() { }; //destruktorius
 };
 
@@ -127,3 +124,5 @@ double vector1(int a, std::string metodas);
 double vector2(int a, std::string metodas);
 double st_part(int a, std::string metodas);
 double partition(int a, std::string metodas);
+
+#endif
